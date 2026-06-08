0bf3ed96903a00e4e6ebbaf68ec1c39e54fba329
Rizz-admin <renamenogame@gmail.com>
2026-06-08 08:54:02 +0300

    pluged in AI

--- vector_search.py
+++ vector_search.py
@@ -1,26 +1,27 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
-# Same embedding model
-embeddings = HuggingFaceEmbeddings(
-    model_name="intfloat/multilingual-e5-base",
-    model_kwargs={"device": "cuda"}
-)
-
-# Load existing DB
-vectorstore = Chroma(
-    persist_directory="./chroma_db",
-    embedding_function=embeddings
-)
 
 # Query
 query = "какие есть вакансии?"
 
-results = vectorstore.similarity_search(
-    query,
-    k=5
-)
+def search(query):
+    # Same embedding model
+    embeddings = HuggingFaceEmbeddings(
+        model_name="intfloat/multilingual-e5-base",
+        model_kwargs={"device": "cpu"}
+    )
 
-for i, doc in enumerate(results, 1):
-    print(f"\n--- Result {i} ---")
-    print(doc.page_content)
+    # Load existing DB
+    vectorstore = Chroma(
+        persist_directory="./chroma_db",
+        embedding_function=embeddings
+    )
+
+    results = vectorstore.similarity_search(
+        query,
+        k=3
+    )
+
+    for i, doc in enumerate(results, 1):
+        return(f"Вопрос пользователя: {query}\n--- Результат {i} ---\n{doc.page_content}")
